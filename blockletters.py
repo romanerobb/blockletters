f868aab07e46cb20dad931fbc766d83c11dba88e
Romane Orlando Robb <75688766+romanerobb@users.noreply.github.com>
2021-08-24 09:44:50 -0400

    Create blockletters.py

---
+++ blockletters.py
@@ -0,0 +1,9 @@
+# Romane Robb
+# Fun Fact: I have created over ten (10) companies.
+print("RRRR    RRRR")
+print("R    R  R    R")
+print("R    R  R    R")
+print("RRRR    RRRR ")
+print("R  R    R  R  ")
+print("R   R   R   R ")
+print("R     R R     R")
